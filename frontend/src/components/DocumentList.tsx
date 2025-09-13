6a48b9ba9abc21c712d096709ae78f1440efb4d6
12221708 <e12221708@student.tuwien.ac.at>
2025-09-13 11:58:07 +0200

    Lifting state up, Documents

--- frontend/src/components/DocumentList.tsx
+++ frontend/src/components/DocumentList.tsx
@@ -2,40 +2,16 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DocumentService, Document } from "../services/DocumentService";
 
+interface DocumentListProps {
+    documents: Document[];
+}
 
-export default function DocumentList() {
-    const [documents, setDocuments] = useState<Document[]>([]);
-    const [search, setSearch] = useState("");
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        async function fetchDocuments() {
-            try {
-                const data = await DocumentService.getDocuments(search);
-
-                if (Array.isArray(data)) {
-                    setDocuments(data);
-                } else {
-                    console.warn("Unexpected response:", data);
-                }
-            } catch (err) {
-                console.error("Failed to fetch documents", err);
-                navigate("/login");
-            }
-        }
-        fetchDocuments();
-    }, [search]);
-
+export default function DocumentList({ documents }: DocumentListProps) {
+    
     return(
         <div className="p-6 max-w-3xl mx-auto">
             <h1 className="text-2xl font-bold mb-4">Lista Dokumenata</h1>
-            <input
-                type="text"
-                placeholder="Pretraži dokumente..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                className="border px-3 py-2 w-full mb-4 rounded"
-            />
+            
             {documents.length == 0 ? (
                 <div className="text-red-600">Nije pronađen nijedan dokument.</div>
             ) : (
--- frontend/src/pages/DocumentsPage.tsx
+++ frontend/src/pages/DocumentsPage.tsx
@@ -1,17 +1,38 @@
 import Button from 'antd/lib/button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DocumentList from '../components/DocumentList';
 import { Modal } from 'antd';
 import DocumentForm from '../components/DocumentForm';
+import { DocumentService, Document } from '../services/DocumentService';
 
 const DocumentsPage = () => {
-    const navigate = useNavigate();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [documents, setDocuments] = useState<Document[]>([]);
+        const [search, setSearch] = useState("");
+        const navigate = useNavigate();
+    
+        useEffect(() => {
+            async function fetchDocuments() {
+                try {
+                    const data = await DocumentService.getDocuments(search);
+    
+                    if (Array.isArray(data)) {
+                        setDocuments(data);
+                    } else {
+                        console.warn("Unexpected response:", data);
+                    }
+                } catch (err) {
+                    console.error("Failed to fetch documents", err);
+                    navigate("/login");
+                }
+            }
+            fetchDocuments();
+        }, [search]);
 
     return (
         <div style={{ padding: '2rem' }}>
-            <DocumentList />
+            <DocumentList documents={documents} />
             <Button type="primary" onClick={() => setIsModalOpen(true)} style={{ marginBottom: '1rem' }}>
                 Dodaj dokument
             </Button>
