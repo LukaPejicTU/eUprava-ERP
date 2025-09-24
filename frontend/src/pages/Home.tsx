c6e87490150480b20dd94e85310f41359720b3d7
lukapejic <e12221708@student.tuwien.ac.at>
2025-09-24 08:46:16 +0200

    Fetching data inside Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
-    return (
-      <div className="text-green-600 text-2xl font-bold underline">
-        Tailwind radi! ✅
-      </div>
-    );
-  }
+import { DashboardData } from "../services/DashboardService"
+import { useState, useEffect } from "react";
+import { DashboardService } from "../services/DashboardService";
+
+const Home = () => {
+  const [dashboardData, setDashboardData] = useState<DashboardData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await DashboardService.getDashboardData();
+        if (data != null) {
+          setDashboardData(data);
+        } else {
+          console.warn("Unexpected response:", data);
+        }
+      } catch (err) {
+        console.error("Failed to fetch dashboard data", err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+}
